71d14296b0546b4bb1c224b2bd4c71bb3f3c28b8
Mostafa Naghipoor <89272100+MostafaNaghipoor82@users.noreply.github.com>
2023-03-18 22:40:31 -0700

    editted

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
-import notes from "./note";
+import notes from "./notes";
 
 function App() {
   return (
