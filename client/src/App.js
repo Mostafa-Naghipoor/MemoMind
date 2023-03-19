f69accda494ba776df633f026a16d2cd995106e7
Mostafa Naghipoor <89272100+MostafaNaghipoor82@users.noreply.github.com>
2023-03-18 22:25:48 -0700

    misspelling corrected

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
-import notes from "../notes";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import notes from "./note";
 
 function App() {
   return (
