2b1be2c74daaad6e4022f254557d24094970f810
Mostafa Naghipoor <89272100+MostafaNaghipoor82@users.noreply.github.com>
2023-03-23 19:06:54 -0700

    add createArea

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import CreateArea from "./components/createArea";
 import notes from "./notes";
+
 
 function App() {
   return (
